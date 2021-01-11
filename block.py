bb5654cc623b8805b73df4a015b224100a3e7e38
dionsaputra <dion.saputra@tiket.com>
2021-01-11 15:09:05 +0700

    restructure code

--- block.py
+++
@@ -1,6 +0,0 @@
-class Block:
-    def __init__(self, row: int, col: int, length: int, horizontal: bool):
-        self.row = row
-        self.col = col
-        self.length = length
-        self.horizontal = horizontal
